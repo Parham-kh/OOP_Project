bf720622db046997f3f64dc75274b55429231e39
Parham <parhamsetar06@gmail.com>
2025-05-19 22:17:55 +0330

    Initializing project

---
+++ test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Asus on 5/19/2025.
+//
+
+#include "test.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  cout << "salam bacheha";
+ }
---
+++ test.h
@@ -0,0 +1,16 @@
+//
+// Created by Asus on 5/19/2025.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //TEST_H
