78363fa2dfd3e6c35ceac4d2eb8ce30e0cd1fa00
Parham <parhamsetar06@gmail.com>
2025-05-20 17:03:29 +0330

    Initializing project

--- main.cpp
+++ main.cpp
@@ -1,14 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Node{};
-class Element{};
-class VoltageSource:public Element{};
-class Resistor:public Element{};
-class Inductor:public Element{};
-class Capacitor:public Element{};
-class CurrentSource:public Element{};
-class Circuit{};
+namespace Model {
+    class Node{};
+    class Element {
+        string type;
+    };
+    class VoltageSource:public Element {
+    };
+    class Resistor:public Element {
+
+    };
+    class Inductor:public Element{};
+    class Capacitor:public Element{};
+    class CurrentSource:public Element{};
+};
+using namespace Model;
+namespace Controller {
+    class Circuit {
+        vector<Element*> elements;
+        vector<Node*> nodes;
+
+    public:
+    };
+}
+using namespace Controller;
+namespace View {
+    class view {
+        public:
+        void run() {
+            string input;
+            getline(cin, input);
+            regex r1("");
+        }
+    };
+}
+using namespace View;
+
 
 int main() {
+    view Views;
+    Views.run();
     return 0;
 }
