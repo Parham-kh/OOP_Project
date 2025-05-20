0bf82fda2a8b87b3732fc9801ed0c1e997d0151a
Parham <parhamsetar06@gmail.com>
2025-05-20 08:11:14 +0330

    Initializing project

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node{};
+class Element{};
+class VoltageSource:public Element{};
+class Resistor:public Element{};
+class Inductor:public Element{};
+class Capacitor:public Element{};
+class CurrentSource:public Element{};
+class Circuit{};
 
 int main() {
     return 0;
