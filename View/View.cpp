dfb496a44db8f931a500ddf3e6a80f3ce35627a7
Parham <parhamsetar06@gmail.com>
2025-05-22 03:22:43 +0330

    Initializing project

--- View/View.cpp
+++ View/View.cpp
@@ -8,7 +8,7 @@ void view::run() {
     getline(cin, input);
     regex r1("add R(\\S+) (\\S+) (\\S+) (\\S+)");
     smatch match;
-    if (regex_search(input, match, r1)) {cout<<1<<endl;
+    if (regex_search(input, match, r1)) {
         circuit->addResistor(match[1].str(), match[2].str(), match[3].str(), stod(match[4].str()));
     }
 }
