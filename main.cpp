f1c2f857d008e2c4eae19739be46a936513cb13a
Parham <parhamsetar06@gmail.com>
2025-05-29 14:26:14 +0330

    Initializing project

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-class ResistorInvalidAmount : public exception {
+class Error : public exception {
+    string message;
+
 public:
+    Error(const string& msg) : message(msg) {}
     const char* what() const noexcept override {
-        return "Error: Resistance cannot be zero or negative";
-    }
-};
-class SyntaxError : public exception {
-public:
-    const char* what() const noexcept override {
-        return "Error: Syntax error";
+        return message.c_str();
     }
 };
 namespace Model {
@@ -40,6 +37,9 @@ namespace Model {
         Element(string name, string type, Node* node1, Node* node2, double voltage,double current)
         : name(name), type(type), node1(node1), node2(node2), voltage(voltage), current(current) {}
         virtual ~Element() = default;
+        string getName() {
+            return name;
+        }
     };
     class VoltageSource:public Element {};
     class Resistor:public Element {
@@ -63,40 +63,68 @@ namespace Controller {
 
     public:
         double Coefficient_management(string a) {
-            regex r1("(\\S*)([k|m|u|n|Meg])");
-            smatch match;
-            if(regex_search(a, match, r1)) {
-                double output = stod(match[1].str());
-                if(match[2]=="k") {
-                    output *=1000;
+            try {
+                regex r1("(\\S*)([k|m|u|n|Meg])");
+                smatch match;
+                if(regex_search(a, match, r1)) {
+                    double output = stod(match[1].str());
+                    if(output <= 0) throw Error("Error: Resistance cannot be zero or negative");
+                    if(match[2]=="k") {
+                        output *=1000;
+                    }
+                    else if(match[2]=="m") {
+                        output *=0.001;
+                    }
+                    else if(match[2]=="u") {
+                        output *=0.000001;
+                    }
+                    else if(match[2]=="n") {
+                        output *=0.000000001;
+                    }
+                    else if(match[2]=="Meg") {
+                        output *=1000000;
+                    }
+                    return output;
                 }
-                else if(match[2]=="m") {
-                    output *=0.001;
+                else {
+                    if(stod(a) <= 0) throw Error("Error: Resistance cannot be zero or negative");
+                    return stod(a);
                 }
-                else if(match[2]=="u") {
-                    output *=0.000001;
-                }
-                else if(match[2]=="n") {
-                    output *=0.000000001;
-                }
-                else if(match[2]=="Meg") {
-                    output *=1000000;
-                }
-                return output;
             }
-            else {
-                return stod(a);
+            catch(const exception& e) {
+                cout<<e.what()<<endl;
             }
         }
         void addResistor(string name,string n1,string n2,string resisrance) {
             try {
+                if(name[0]!='R') {
+                    throw Error("Error: Element "+name+" not found in library");
+                }
+                for(auto e:elements) {
+                    if(e->getName()==name) {
+                        throw Error("Error: Resistor "+name+" already exists in the circuit");
+                    }
+                }
                 Node* node1=new Node(n1,0);
                 Node* node2=new Node(n2,0);
                 nodes.push_back(node1);
+                nodes.push_back(node2);
                 Resistor* resistor=new Resistor(name,node1,node2,Coefficient_management(resisrance));
                 elements.push_back(resistor);
             }catch(const exception& e) {
                 cout<<e.what()<<endl;
+            }
+        }
+        void deleteResistor(string name) {
+            bool found=false;
+            for(int i=0;i<elements.size();i++) {
+                if(elements[i]->getName()=="R"+name) {
+                    elements.erase(elements.begin()+i);
+                    found=true;
+                }
+            }
+            if(!found) {
+            throw Error("Error: Cannot delete resistor; component not found");
             }
         }
     };
@@ -110,12 +138,26 @@ class view {
     public:
     view(Circuit* c) : circuit(c) {}
     void run() {
-        string input;
-        getline(cin, input);
-        regex r1("add R(\\S+) (\\S+) (\\S+) (\\S+)");
-        smatch match;
-        if (regex_search(input, match, r1)) {
-            circuit->addResistor(match[1].str(), match[2].str(), match[3].str(), match[4].str());
+        while(true){
+            try {
+                string input;
+                getline(cin, input);
+                regex r1("add (\\S+) (\\S+) (\\S+) (\\S+)");
+                regex r2("delete (\\S+)");
+                smatch match;
+                if (regex_search(input, match, r1)) {
+                    circuit->addResistor(match[1].str(), match[2].str(), match[3].str(), match[4].str());
+                }
+                else if (regex_search(input, match, r2)) {
+                    circuit->deleteResistor(match[1].str());
+                }
+                else {
+                    throw Error("Error: Syntax error");
+                }
+            }
+            catch(const exception& e) {
+                cout<<e.what()<<endl;
+            }
         }
     }
 };
