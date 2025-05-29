af4c35ab7b1e2e35d2ef018b20a837d729ad42e3
Parham <parhamsetar06@gmail.com>
2025-05-29 16:17:31 +0330

    Initializing project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
 class Error : public exception {
     string message;
 
@@ -10,20 +10,28 @@ public:
         return message.c_str();
     }
 };
+
 namespace Model {
     class Node {
         private:
         string name="";
         double voltage=0;
+        bool IS_GND=false;
 
         public:
         Node(string name, double voltage) :name(name),voltage(voltage){}
         string getName() {
             return name;
         }
+        void setGND(bool IS_GND) {
+            this->IS_GND = IS_GND;
+        }
+        bool isGND() {
+            return this->IS_GND;
+        }
     };
+
     class Element {
-        private:
         Node* node1;
         Node* node2;
 
@@ -33,37 +41,58 @@ namespace Model {
         double voltage=0;
         double current=0;
 
-        public:
+    public:
         Element(string name, string type, Node* node1, Node* node2, double voltage,double current)
         : name(name), type(type), node1(node1), node2(node2), voltage(voltage), current(current) {}
-        virtual ~Element() = default;
-        string getName() {
+        string getName() const {
             return name;
         }
+        string getType() const {
+            return type;
+        }
+        virtual ~Element() = default;
     };
-    class VoltageSource:public Element {};
-    class Resistor:public Element {
-        private:
-        double resistance=0;
 
-        public:
+    class VoltageSource:public Element {};
+
+    class Resistor:public Element {
+        double resistance = 0;
+
+    public:
         Resistor(string name, Node* node1, Node* node2, double resistance)
         : Element(name, "Resistor", node1, node2, 0, 0), resistance(resistance) {}
-        double getResistance() {return resistance;}
     };
-    class Inductor:public Element{};
-    class Capacitor:public Element{};
+
+    class Inductor:public Element{
+        double inductance = 0;
+
+    public:
+        Inductor(string name, Node* node1, Node* node2, double inductance)
+        : Element(name, "Resistor", node1, node2, 0, 0), inductance(inductance) {}
+    };
+
+    class Capacitor : public Element {
+        double capacitance = 0;
+
+    public:
+        Capacitor(string name, Node* node1, Node* node2, double capacitance)
+                : Element(name, "Resistor", node1, node2, 0, 0), capacitance(capacitance) {}
+        double getCapacitance() {
+            return capacitance;
+        }
+    };
+
     class CurrentSource:public Element{};
     class Diode:public Element {
-        private:
         string model="";
 
-        public:
+    public:
         Diode(string name, Node* node1, Node* node2, string model)
         : Element(name, "Diode", node1, node2, 0, 0), model(model) {}
     };
 };
 using namespace Model;
+
 namespace Controller {
     class Circuit {
         vector<Element*> elements;
@@ -76,7 +105,8 @@ namespace Controller {
                 smatch match;
                 if(regex_search(a, match, r1)) {
                     double output = stod(match[1].str());
-                    if(output <= 0) throw Error("Error: Resistance cannot be zero or negative");
+                    if(output <= 0)
+                        throw Error("Error: Resistance cannot be zero or negative");
                     if(match[2]=="k") {
                         output *=1000;
                     }
@@ -95,7 +125,8 @@ namespace Controller {
                     return output;
                 }
                 else {
-                    if(stod(a) <= 0) throw Error("Error: Resistance cannot be zero or negative");
+                    if(stod(a) <= 0)
+                        throw Error("Error: Resistance cannot be zero or negative");
                     return stod(a);
                 }
             }
@@ -103,26 +134,151 @@ namespace Controller {
                 cout<<e.what()<<endl;
             }
         }
+
         void addResistor(string name,string n1,string n2,string resisrance) {
             try {
-                if(name[0]!='R') {
-                    throw Error("Error: Element "+name+" not found in library");
-                }
                 for(auto e:elements) {
                     if(e->getName()==name) {
                         throw Error("Error: Resistor "+name+" already exists in the circuit");
                     }
                 }
-                Node* node1=new Node(n1,0);
-                Node* node2=new Node(n2,0);
-                nodes.push_back(node1);
-                nodes.push_back(node2);
+                Node* node1;
+                Node* node2;
+                bool nod1=false,nod2=false;
+                for(auto n:nodes) {
+                    if(n->getName()==n1) {
+                        nod1=true;
+                        node1=n;
+                    }
+                    if(n->getName()==n2) {
+                        nod2=true;
+                        node2=n;
+                    }
+                }
+                if(!nod1) {
+                    Node* node1=new Node(n1,0);
+                    nodes.push_back(node1);
+                }
+                if(!nod2) {
+                    Node* node2=new Node(n2,0);
+                    nodes.push_back(node2);
+                }
                 Resistor* resistor=new Resistor(name,node1,node2,Coefficient_management(resisrance));
                 elements.push_back(resistor);
             }catch(const exception& e) {
                 cout<<e.what()<<endl;
             }
         }
+
+        void deleteResistor(string name) {
+            bool found=false;
+            for(int i=0;i<elements.size();i++) {
+                if(elements[i]->getName() == name) {
+                    elements.erase(elements.begin()+i);
+                    found=true;
+                }
+            }
+            if(!found) {
+                throw Error("Error: Cannot delete resistor; component not found");
+            }
+        }
+
+        void addCapacitor(string name,string n1,string n2,string capacitance) {
+            try {
+                for(auto e : elements) {
+                    if(e->getName() == name) {
+                        throw Error("Error: Capacitor "+ name +" already exists in the circuit");
+                    }
+                }
+                Node* node1;
+                Node* node2;
+                bool nod1=false,nod2=false;
+                for(auto n:nodes) {
+                    if(n->getName()==n1) {
+                        nod1=true;
+                        node1=n;
+                    }
+                    if(n->getName()==n2) {
+                        nod2=true;
+                        node2=n;
+                    }
+                }
+                if(!nod1) {
+                    Node* node1=new Node(n1,0);
+                    nodes.push_back(node1);
+                }
+                if(!nod2) {
+                    Node* node2=new Node(n2,0);
+                    nodes.push_back(node2);
+                }
+                Capacitor* capacitor = new Capacitor(name, node1, node2, Coefficient_management(capacitance));
+                elements.push_back(capacitor);
+            }catch(const exception& e) {
+                cout << e.what() << endl;
+            }
+        }
+
+        void deleteCapacitor(string name) {
+            bool found = false;
+            for(int i=0; i < elements.size(); i++) {
+                if(elements[i]->getName() == name) {
+                    elements.erase(elements.begin() + i);
+                    found = true;
+                }
+            }
+            if(!found) {
+                throw Error("Error: Cannot delete capacitor; component not found");
+            }
+        }
+
+        void addInductor(string name,string n1,string n2,string inductance) {
+            try {
+                for(auto e : elements) {
+                    if(e->getName() == name) {
+                        throw Error("Error: Inductor "+ name +" already exists in the circuit");
+                    }
+                }
+                Node* node1;
+                Node* node2;
+                bool nod1=false,nod2=false;
+                for(auto n:nodes) {
+                    if(n->getName()==n1) {
+                        nod1=true;
+                        node1=n;
+                    }
+                    if(n->getName()==n2) {
+                        nod2=true;
+                        node2=n;
+                    }
+                }
+                if(!nod1) {
+                    Node* node1=new Node(n1,0);
+                    nodes.push_back(node1);
+                }
+                if(!nod2) {
+                    Node* node2=new Node(n2,0);
+                    nodes.push_back(node2);
+                }
+                Inductor* inductor = new Inductor(name, node1, node2, Coefficient_management(inductance));
+                elements.push_back(inductor);
+            }catch(const exception& e) {
+                cout << e.what() << endl;
+            }
+        }
+
+        void deleteInductor(string name) {
+            bool found = false;
+            for(int i=0; i < elements.size(); i++) {
+                if(elements[i]->getName() == name) {
+                    elements.erase(elements.begin() + i);
+                    found = true;
+                }
+            }
+            if(!found) {
+                throw Error("Error: Cannot delete inductor; component not found");
+            }
+        }
+
         void addDiode(string name,string n1,string n2,string model) {
             try {
                 for(auto e:elements) {
@@ -157,18 +313,7 @@ namespace Controller {
                 cout<<e.what()<<endl;
             }
         }
-        void deleteResistor(string name) {
-            bool found=false;
-            for(int i=0;i<elements.size();i++) {
-                if(elements[i]->getName()==name) {
-                    elements.erase(elements.begin()+i);
-                    found=true;
-                }
-            }
-            if(!found) {
-            throw Error("Error: Cannot delete resistor; component not found");
-            }
-        }
+
         void deleteDiode(string name) {
             bool found=false;
             for(int i=0;i<elements.size();i++) {
@@ -181,46 +326,105 @@ namespace Controller {
                 throw Error("Error: Cannot delete diode; component not found");
             }
         }
-    };
-}
-using namespace Controller;
-namespace View {
-class view {
-    private:
-    Circuit* circuit;
 
-    public:
-    view(Circuit* c) : circuit(c) {}
-    void run() {
-        while(true){
-            try {
-                string input;
-                getline(cin, input);
-                regex r1("add (\\S+) (\\S+) (\\S+) (\\S+)");
-                regex r2("delete (\\S+)");
-                regex Add_Diode("add (\\S+) (\\S+) (\\S+) (\\S+)");
-                regex Delete_Diode("delete (\\S+)");
-                smatch match;
-                if (regex_search(input, match, r1)) {
-                    if(match[1][0]=)
-                    circuit->addResistor(match[1].str(), match[2].str(), match[3].str(), match[4].str());
-                }
-                else if (regex_search(input, match, r2)) {
-                    circuit->deleteResistor(match[1].str());
-                }
-                else if (regex_search(input, match, Add_Diode)) {}
-                else if (regex_search(input, match, Delete_Diode)) {}
-                else {
-                    throw Error("Error: Syntax error");
+        void add_GND(string node) {
+            bool found=false;
+            for(auto n:nodes) {
+                if(n->getName()==node) {
+                    n->setGND(true);
+                    found=true;
                 }
             }
-            catch(const exception& e) {
-                cout<<e.what()<<endl;
+            if(!found) {
+                cout<<"Node does not exist"<<endl;
             }
         }
-    }
-};
+
+        void delete_GND(string node) {
+            bool found=false;
+            for(auto n:nodes) {
+                if(n->getName()==node) {
+                    n->setGND(false);
+                    found=true;
+                }
+            }
+            if(!found) {
+                cout<<"Node does not exist"<<endl;
+            }
+        }
+    };
 }
+
+using namespace Controller;
+
+namespace View {
+    class view {
+    private:
+        Circuit *circuit;
+
+    public:
+        view(Circuit *c) : circuit(c) {}
+
+        void run() {
+            while(true){
+                try {
+                    string input;
+                    getline(cin, input);
+                    regex add("add (\\S+) (\\S+) (\\S+) (\\S+)");
+                    regex del("delete (\\S+)");
+                    regex add_GND("add (\\S+) (\\S+)");
+                    regex Delete_GND("delete (\\S+) (\\S+)");
+
+                    smatch match;
+                    if (regex_search(input, match, add)) {
+                        if (match[1].str()[0] == 'R')
+                            circuit->addResistor(match[1].str(), match[2].str(), match[3].str(), match[4].str());
+                        else if (match[1].str()[0] == 'C')
+                            circuit->addCapacitor(match[1].str(), match[2].str(), match[3].str(), match[4].str());
+                        else if (match[1].str()[0] == 'L')
+                            circuit->addInductor(match[1].str(), match[2].str(), match[3].str(), match[4].str());
+                        else if (match[1].str()[0] == 'D')
+                            circuit->addDiode(match[1].str(), match[2].str(), match[3].str(), match[4].str());
+                        else
+                            throw Error("Error: Element "+ match[1].str() + " not found in library");
+                    }
+                    else if (regex_search(input, match, del)) {
+                        if (match[1].str()[0] == 'R')
+                            circuit->deleteResistor(match[1].str());
+                        else if (match[1].str()[0] == 'C')
+                            circuit->deleteCapacitor(match[1].str());
+                        else if (match[1].str()[0] == 'L')
+                            circuit->deleteInductor(match[1].str());
+                        else if (match[1].str()[0] == 'D')
+                            circuit->deleteDiode(match[1].str());
+                        else
+                            throw Error("Error: Element "+ match[1].str() + " not found in library");
+                    }
+                    else if(regex_search(input, match, add_GND)) {
+                        if(match[1].str() == "GND")
+                            circuit->add_GND(match[2].str());
+                        else
+                            throw Error("Error: Element "+match[1].str() + " not found in library");
+                    }
+                    else if(regex_search(input, match, Delete_GND)) {
+                        if(match[1].str() == "GND")
+                            circuit->delete_GND(match[2].str());
+                        else
+                            throw Error("Error: Element "+match[1].str() + " not found in library");
+                    }
+                    else {
+                        throw Error("Error: Syntax error");
+                    }
+                }
+                catch(const exception& e) {
+                    cout<<e.what()<<endl;
+                }
+            }
+        }
+    };
+}
+
+
 using namespace View;
 
 int main() {
