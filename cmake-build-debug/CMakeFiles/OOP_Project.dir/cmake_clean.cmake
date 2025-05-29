cb2fb29c95821daad04fed57209468b6dff5a489
TheGodSlayer13 <kratosamir1385@gmail.com>
2025-05-29 15:36:30 +0330

    second commit

---
+++ cmake-build-debug/CMakeFiles/OOP_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Project.dir/main.cpp.obj"
+  "CMakeFiles/OOP_Project.dir/main.cpp.obj.d"
+  "OOP_Project.exe"
+  "OOP_Project.exe.manifest"
+  "OOP_Project.pdb"
+  "libOOP_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OOP_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OOP_Project.
+# This may be replaced when dependencies are built.
